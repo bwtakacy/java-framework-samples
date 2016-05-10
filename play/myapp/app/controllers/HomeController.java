d07eaa7af4b47e6a918f21e43f60d73b1b41453a
bwtakacy <bwtakacy@gmail.com>
2016-05-10 22:52:45 +0900

    Use Form sample

--- play/myapp/app/controllers/HomeController.java
+++ play/myapp/app/controllers/HomeController.java
@@ -1,30 +1,43 @@
 package controllers;
 
+import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import static play.data.Form.*;
 import views.html.*;
+import play.data.validation.Constraints.*;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
+
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    public static class MyForm {
+        @Required
+        public String name;
+        public String mail;
+        public String tel;
+
+        public String toString() {
+            return "NAME:" + name + " MAIL:" + mail + " TEL:" + tel;
+        }
+    }
+
     public Result index() {
         String title = "Play Sample Page";
-        String msg = "Playの世界へようこそ";
-        String[] contents = new String[] {
-            "PlayはJSP/サーブレットとは異なる新しいフレームワークです。",
-            "Scalaという言語を用いて簡単にコーディングできます。",
-            "HerokuなどのIaaSにも簡単にデプロイできます。"
-        };
-        return ok(index.render(title, msg, contents));
+        String msg = "フォームのサンプルです。";
+        return ok(index.render(title, msg, form(MyForm.class)));
+    }
+
+    public Result sendform() {
+        String title = "Play Send Form Page";
+        Form<MyForm> form = form(MyForm.class).bindFromRequest();
+        if (form.hasErrors()) {
+            String msg = "入力に問題があります。";
+            return badRequest(index.render(title, msg, form));
+        } else {
+            MyForm data = form.get();
+            String msg = data.toString();
+            return ok(index.render(title, msg, form));
+        }
     }
 
 }
